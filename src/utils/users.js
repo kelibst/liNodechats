8c2c8fa523219d12cfca18e4569eca3f13855856
Keli Booster <kbooster17@gmail.com>
2022-06-04 20:58:27 +0000

    add ability to store user data

---
+++ src/utils/users.js
@@ -0,0 +1,73 @@
+const users = [];
+
+const addUser = ({ id, username, room }) => {
+  username = username.trim().toLowerCase();
+  room = room.trim().toLowerCase();
+
+  if (!username || !room) {
+    return {
+      error: "You need to provide a valid user and room",
+    };
+  }
+
+  const exUser = users.find((user) => {
+    return user.username === username && user.room === room;
+  });
+
+  if (exUser) {
+    return {
+      error: "Sorry that username is already in use",
+    };
+  }
+
+  const user = {
+    id,
+    username,
+    room,
+  };
+  users.push(user);
+
+  return {
+    user,
+  };
+};
+
+const removeUser = (id) => {
+  const userInd = users.findIndex((user) => user.id === id);
+
+  if (userInd !== -1) {
+    return users.splice(userInd, 1)[0];
+  }
+};
+
+const getUser = (id) => {
+  const user = users.find((usr) => usr.id === id);
+
+  //   if (!user) {
+  //     return {
+  //       error: "That user does not exist!",
+  //     };
+  //   }
+
+  return {
+    user,
+  };
+};
+
+const getUserinRoom = (room) => {
+  const romUser = users.filter((usr) => usr.room === room);
+  //   if (!romUser?.length) {
+  //     return {
+  //       error: "There are no users in the room!",
+  //     };
+  //   }
+
+  return romUser;
+};
+
+module.exports = {
+  getUser,
+  getUserinRoom,
+  removeUser,
+  addUser,
+};
